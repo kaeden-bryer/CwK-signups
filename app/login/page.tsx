afd6bb2a003656108eecb8e1b53f36bd8eab0f1a
Kaeden Bryer <130073365+kaeden-bryer@users.noreply.github.com>
2026-03-06 20:41:58 -0500

    strange error

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { LoginForm } from "./login-form";
+import { LoginForm } from "@/app/login/login-form";
 
 export default function LoginPage() {
   return (
